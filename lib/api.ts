6b08266ab14861e4dcede8f3ab69c6295a4ee5ae
gilbertmunuotz <officialgilbert24@gmail.com>
2026-03-10 17:01:34 +0300

    #feat: introduced global API handler interceptor

---
+++ lib/api.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { SERVER_URI } from "@/constants/constant";
+import { auth } from "@/auth";
+
+export const api = axios.create({
+    baseURL: `${SERVER_URI}/api/v1`,
+});
+
+// Request interceptor: attach the latest token
+api.interceptors.request.use(async (config) => {
+    const session = await auth(); // always get latest session
+    const token = session?.user?.accessToken;
+
+    if (token && config.headers) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+// Response interceptor: just log the refreshed token
+api.interceptors.response.use(
+    (response) => {
+        const refreshed = response.headers["x-refreshed-token"];
+
+        if (refreshed) {
+            console.log("refreshed token:", refreshed);
+        }
+
+        return response;
+    },
+    (error) => Promise.reject(error)
+);
